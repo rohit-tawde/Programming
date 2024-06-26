5e49525cbe81602160267186daef0017b9f84de6
rohit-tawde <mr.rohit.tawde@gmail.com>
2024-06-26 21:50:46 +0530

    Diffrence between Integer and Float

---
+++ Conditional/DifFloatInt.java
@@ -0,0 +1,6 @@
+public class DifFloatInt {
+    public static void main(String[] args) {
+        System.out.println(9 / 5);    // Integer division: results in 1
+        System.out.println(9.0 / 5);  // Floating-point division: results in 1.8
+    }
+}
