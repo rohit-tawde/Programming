16b61f75b07300d4d1f8859386ae7fc984cdd495
rohit-tawde <mr.rohit.tawde@gmail.com>
2024-07-15 13:25:05 +0530

    Update

---
+++ patterns/star/pattern7.java
@@ -0,0 +1,30 @@
+class pattern7 {
+    public static void main(String[] args) {
+
+        int num = 4;
+/*
+
+* * * * * * * 
+  * * * * *   
+    * * *     
+      *       
+    * * *     
+  * * * * *   
+* * * * * * * 
+
+*/
+        for(int i=1; i<num*2; i++) {
+            for(int j=1; j<num * 2; j++) {
+
+                if(j>=i && i+j<=num * 2 || i+j>=num*2 && i>=j) {
+
+                System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
